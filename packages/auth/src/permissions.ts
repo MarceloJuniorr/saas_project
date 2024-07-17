17eade3b7ff3f74dd5ce61b55e744cb3a705fa36
Marcelo Junior <marcelojunior.07@live.com>
2024-07-17 17:26:38 -0300

    update alter in mateus pc

---
+++ packages/auth/src/permissions.ts
@@ -0,0 +1,15 @@
+import { AbilityBuilder } from '@casl/ability';
+import { AppAbility } from '.'
+
+type Role= 'ADMIN' | 'MEMBER'
+
+type PermissionsByRole = (user: any, builder: AbilityBuilder<AppAbility>) => void
+
+export const permissions: Record<Role, PermissionsByRole> = { 
+    ADMIN(_, {can}) {
+        can('manage', 'all')
+    },
+    MEMBER(_, {can}) {
+        can('invite', 'User')
+    }
+}
